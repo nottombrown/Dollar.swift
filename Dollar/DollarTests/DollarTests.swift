b64a5429e051aac04d285da85badcaf5a4583fa5
Tom Brown <tom@joingrouper.com>
2014-07-07 22:19:43 -0700

    Failing test

--- Dollar/DollarTests/DollarTests.swift
+++ Dollar/DollarTests/DollarTests.swift
@@ -27,6 +27,6 @@ class DollarTests: XCTestCase {
     }
 
     func testFirst() {
-        XCTAssertEqualObjects(first([1, 2, 3, 4])!, 1, "Return first element")
+        XCTAssertEqualObjects(first([1, 2, 3, 4, 5, 6, 7, 8, 9])!, 1, "Return first element")
     }
 }
